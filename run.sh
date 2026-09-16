2cb0e52863f4cb73423bc98aef47ce53305cf1ab
Max Last <tsalxam@gmail.com>
2026-09-16 10:40:06 +0000

    stereo and point reduction tests

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# python scripts/run_inference.py --input examples/img_4425_pointreduction_25.mp4 --output_dir outputs/img_4425_pointreduction_25
+# python scripts/run_inference.py --input examples/img_4425_stereo.mp4 --output_dir outputs/img_4425_stereo
+# python scripts/run_inference.py --input examples/img_4425_pointreduction_5.mp4 --output_dir outputs/img_4425_pointreduction_5
