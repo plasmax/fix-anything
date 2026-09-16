13abf34bf883ec0d03e21799d4fbd1b9887145a3
Max Last <tsalxam@gmail.com>
2026-09-16 11:00:14 +0000

    reduced points greatly

--- run.sh
+++ run.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 # python scripts/run_inference.py --input examples/img_4425_pointreduction_25.mp4 --output_dir outputs/img_4425_pointreduction_25
 # python scripts/run_inference.py --input examples/img_4425_stereo.mp4 --output_dir outputs/img_4425_stereo
 # python scripts/run_inference.py --input examples/img_4425_pointreduction_5.mp4 --output_dir outputs/img_4425_pointreduction_5
+# python scripts/run_inference.py --input examples/img_4425_pointreduction_1.mp4 --output_dir outputs/img_4425_pointreduction_1
